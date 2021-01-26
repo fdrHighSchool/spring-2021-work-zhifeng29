3babbfef6e25c42ea6f84aad1f0f1f55f11b42d2
zhi <>
2021-01-25 21:22:33 -0500

    finish

--- 00-SuperArray/SuperArray.java
+++ 00-SuperArray/SuperArray.java
@@ -1,33 +1,97 @@
+import java.util.Arrays;
 public class SuperArray {
   // instance variables
   private int[] arr;
+  private int index;
+  private int val;
 
   //------------------------------------------------//
   // constructor
   public SuperArray() {
     this.arr = new int[10];
+    this.index = 0;
+
+
+
   }
   public SuperArray(int l) {
     this.arr = new int[l];
+    this.index = 0;
+
+
   }//end constructor method
   //------------------------------------------------//
-  public String getName() {
-    return this.name;
-  }//end getName method
+  public void add (int index,int val) {
+    int[] temp = new int[this.arr.length+1];
+    int[] temp2 = Arrays.copyOfRange(this.arr,0,index);
+    int[] temp3 = Arrays.copyOfRange(this.arr,index, this.arr.length);
 
-  public int getCurrentage() {
-    return this.currentage;
-  }//end getAgelimit method
+    for (int i = 0;i < temp2.length;i++){
+      temp[i] = temp2[i];
+      this.index++;
+    }
 
-  public int getAgelimit() {
-    return this.agelimit;
-  }//end getAgelimit method
-  public double percent() {
-    double ans = (double)this.currentage/(double)this.agelimit;
-    return ans;
-  }//end getAgelimit method
+    temp[this.index] = val;
+    this.index++;
 
-  public String toString() {
-    return "Name: " + this.name + " CurrentAge: " + this.currentage + " Agelimit: " + this.agelimit;
+    for (int i = 0;i < temp3.length;i++){
+      temp[i + this.index] = temp3[i];
+    }
+
+    this.arr = temp;
+  }//end of add
+  public void add (int val) {
+    for(int i = 0; i < arr.length;i++){
+      if(this.arr[i] == 0){
+        this.arr[i] = val;
+        return;
+      }
+    }
+    int[] temp = Arrays.copyOf(this.arr,this.arr.length + 1 );
+    temp[temp.length-1] = val;
+    this.arr = temp;
+  }//end of add
+
+  public void set (int index,int val) {
+    this.arr[index] = val;
+  }
+
+  public void grow (int n) {
+    int[] temp = Arrays.copyOf(this.arr,this.arr.length + n );
+    this.arr = temp;
+  }
+
+  public boolean isEmpty () {
+    for(int i = 0; i < arr.length;i++){
+      if(this.arr[i] != 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public void remove (int index) {
+    int[] temp = new int[this.arr.length-1];
+    int[] temp2 = Arrays.copyOfRange(this.arr,0,index);
+    int[] temp3 = Arrays.copyOfRange(this.arr,index+1, this.arr.length);
+
+    for (int i = 0;i < temp2.length;i++){
+      temp[i] = temp2[i];
+      this.index++;
+    }
+    for (int i = 0;i < temp3.length;i++){
+      temp[i + this.index] = temp3[i];
+    }
+    this.arr = temp;
+  }//end of remove
+
+  public void display() {
+    System.out.println(Arrays.toString(this.arr));
+  }
+
+  //public String toString() {}
+
+
+
 
   }//end toString method
--- 00-SuperArray/SuperArrayDriver.java
+++ 00-SuperArray/SuperArrayDriver.java
@@ -1,5 +1,27 @@
 public class SuperArrayDriver {
   public static void main(String[] args) {
+    SuperArray array = new SuperArray();
+    SuperArray array2 = new SuperArray(5);
+    array.display();
+    for (int i = 0;i < 10;i++){
+      array.set(i,i);
+    }
 
+    System.out.println(array.isEmpty());
+    array.grow(2);
+    array.display();
+    array.remove(4);
+    array.display();
+    for (int i = 0;i < 5;i++){
+      array.add(78);
+    }
+    array.display();
+    System.out.println("here");
+
+    for (int i = 0;i < 5;i++){
+      array2.set(i,i);
+    }
+    array2.add(1,100);
+    array2.display();
   }//end of main
 }//end of class
