72e15503f7a356a9f020ee1bbbe7c594b4340a47
zhi <>
2021-03-01 18:27:01 -0500

    point1 finish

--- 01-point/Point.java
+++ 01-point/Point.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.lang.Math;
 public class Point {
   // instance variables
   private int x;
@@ -42,5 +43,46 @@ public class Point {
  // shifts this point's location by the given amount
  public void translate(int dx, int dy) {
    setLocation(x + dx, y + dy);
+ }
+
+ public double distance(int dx,int dy) {
+   if(x==0){
+     this.x=1;
+   }
+   if(y==0){
+     this.y=1;
+   }
+   if(dx==0){
+     dx=1;
+   }
+   if(dy==0){
+     dy=1;
+   }
+   return Math.sqrt(Math.abs(this.x * this.x * dx * dx) + Math.abs(this.y * this.y * dy * dy));
+ }
+
+ public String midpoint(double x2,double y2) {
+   x2 = (x2 + x)/2;
+   y2 = (y2 + y)/2;
+
+   return "(" + x2 + ", " + y2 + ")";
+ }
+ public String slope(int x2,int y2) {
+   y2=y2-(y);
+   x2=x2-(x);
+
+   return   x2 + "/" + y2  ;
+ }
+ public boolean isCollinear(int x2,int y2,int x3,int y3){
+   if (x == 0 && x2 == 0 && x3 == 0){
+     return true;
+   }
+   if (y == 0 && y2 == 0 && y3 == 0){
+     return true;
+   }
+   if(x/y == x2/y2 && x/y == x3/y3){
+     return true;
+   }
+   return false;
  }
 }//end of class
--- 01-point/PointDriver.java
+++ 01-point/PointDriver.java
@@ -2,9 +2,14 @@ public class PointDriver {
   public static void main(String[] args) {
 
     Point point1 = new Point();
-    Point point2 = new Point(5,5);
+    Point point2 = new Point(1,2);
 
     System.out.print(point1);
-    System.out.print(point2);
+    System.out.println(point2);
+    System.out.println(point2.distanceFromOrigin());
+    System.out.println(point1.distance(5,5));
+    System.out.println(point2.midpoint(6,2));
+    System.out.println(point2.slope(6,2));
+    System.out.println(point2.isCollinear(2,4,3,6));
   }//end of main
 }//end of class
